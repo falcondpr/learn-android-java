6bb317bf4446c517dcb68ce9e6c3095067acb8bf
Fernando Villalba <21fervillalbag@gmail.com>
2024-03-26 16:42:49 -0400

    feat: practice with checkboxs

--- app/src/main/java/com/example/proyectoprueba/MainActivity.java
+++ app/src/main/java/com/example/proyectoprueba/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.proyectoprueba;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -10,8 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private EditText et1, et2;
-    private RadioButton rb1, rb2;
     private TextView textResult;
+    private CheckBox check1, check2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,25 +21,29 @@ public class MainActivity extends AppCompatActivity {
 
         et1 = findViewById(R.id.et1);
         et2 = findViewById(R.id.et2);
-        rb1 = findViewById(R.id.rb1);
-        rb2 = findViewById(R.id.rb2);
+
+        check1 = findViewById(R.id.check1);
+        check2 = findViewById(R.id.check2);
 
         textResult = findViewById(R.id.textResult);
     }
 
     public void calculateOperation(View v) {
-        String s1 = et1.getText().toString();
-        String s2 = et2.getText().toString();
+        int v1 = Integer.parseInt(et1.getText().toString());
+        int v2 = Integer.parseInt(et2.getText().toString());
 
-        int v1 = Integer.parseInt(s1);
-        int v2 = Integer.parseInt(s2);
+        String result = "";
 
-        if (rb1.isChecked()) {
-            int sumando = v1 + v2;
-            textResult.setText("La suma es: " + sumando);
-        } else if (rb2.isChecked()) {
-            int restando = v1 - v2;
-            textResult.setText("La resta es: " + restando);
+        if (check1.isChecked()) {
+            int suma = v1 + v2;
+            result += "La suma es: " + suma + " ";
         }
+
+        if (check2.isChecked()) {
+            int resta = v1 - v2;
+            result += " La resta es: " + resta;
+        }
+
+        textResult.setText(result);
     }
 }
