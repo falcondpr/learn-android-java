e6e0c82713af7f88ff45e72cffc44134f1ce49c5
Fernando Villalba <21fervillalbag@gmail.com>
2024-03-26 15:51:20 -0400

    feat: some inputs

--- app/src/main/java/com/example/proyectoprueba/MainActivity.java
+++ app/src/main/java/com/example/proyectoprueba/MainActivity.java
@@ -3,14 +3,15 @@ package com.example.proyectoprueba;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private EditText et1, et2;
-    private TextView txt1;
-
+    private RadioButton rb1, rb2;
+    private TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +20,25 @@ public class MainActivity extends AppCompatActivity {
 
         et1 = findViewById(R.id.et1);
         et2 = findViewById(R.id.et2);
-        txt1 = findViewById(R.id.txt1);
+        rb1 = findViewById(R.id.rb1);
+        rb2 = findViewById(R.id.rb2);
+
+        textResult = findViewById(R.id.textResult);
     }
 
-    public void sumNums(View v) {
+    public void calculateOperation(View v) {
         String s1 = et1.getText().toString();
         String s2 = et2.getText().toString();
+
         int v1 = Integer.parseInt(s1);
         int v2 = Integer.parseInt(s2);
-        int suma = v1 + v2;
-        txt1.setText("La suma es: "+ suma);
+
+        if (rb1.isChecked()) {
+            int sumando = v1 + v2;
+            textResult.setText("La suma es: " + sumando);
+        } else if (rb2.isChecked()) {
+            int restando = v1 - v2;
+            textResult.setText("La resta es: " + restando);
+        }
     }
 }
