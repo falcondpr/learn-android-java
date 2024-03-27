f4566b4dd49b712b9266e02e1d1e706cfe1408c2
Fernando Villalba <21fervillalbag@gmail.com>
2024-03-27 17:09:14 -0400

    feat: spinner component

--- app/src/main/java/com/example/proyectoprueba/MainActivity.java
+++ app/src/main/java/com/example/proyectoprueba/MainActivity.java
@@ -2,33 +2,58 @@ package com.example.proyectoprueba;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageButton image1, image2, image3, image4;
+    private Spinner spinner1;
+    private TextView txtResult;
+    private EditText i1, i2;
+    private String[] operations = {"sumar", "restar", "multiplicar", "dividir"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        i1 = findViewById(R.id.input1);
+        i2 = findViewById(R.id.input2);
+
+        txtResult = findViewById(R.id.textResult);
+        spinner1 = findViewById(R.id.spinner1);
+
+        ArrayAdapter<String>adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, operations);
+        spinner1.setAdapter(adapter1);
     }
 
-    public void press(View v) {
-        ImageButton imgBtn = (ImageButton)v;
-        imgBtn.setVisibility(View.INVISIBLE);
-    }
+    public void operate(View v) {
+        int value1 = Integer.parseInt(i1.getText().toString());
+        int value2 = Integer.parseInt(i2.getText().toString());
 
-    public void restart(View v) {
-        ImageButton image1 = findViewById(R.id.image1);
-        ImageButton image2 = findViewById(R.id.image2);
-        ImageButton image3 = findViewById(R.id.image3);
-        ImageButton image4 = findViewById(R.id.image4);
+        String operation = spinner1.getSelectedItem().toString();
 
-        image1.setVisibility(View.VISIBLE);
-        image2.setVisibility(View.VISIBLE);
-        image3.setVisibility(View.VISIBLE);
-        image4.setVisibility(View.VISIBLE);
+        if (operation.equals("sumar")) {
+            int suma = value1 + value2;
+            txtResult.setText("La suma es: " + suma);
+        }
+
+        if (operation.equals("restar")) {
+            int resta = value1 - value2;
+            txtResult.setText("La resta es: " + resta);
+        }
+
+        if (operation.equals("multiplicar")) {
+            int multiplicacion = value1 * value2;
+            txtResult.setText("La multiplicacion es: " + multiplicacion);
+        }
+
+        if (operation.equals("dividir")) {
+            int division = value1 / value2;
+            txtResult.setText("La division es: " + division);
+        }
     }
 }
