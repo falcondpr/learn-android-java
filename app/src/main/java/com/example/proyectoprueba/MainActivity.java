9a68c2c8c5c7620f5e011cacf3ccb76160df0675
Fernando Villalba <21fervillalbag@gmail.com>
2024-03-27 15:55:39 -0400

    feat: frame layout

--- app/src/main/java/com/example/proyectoprueba/MainActivity.java
+++ app/src/main/java/com/example/proyectoprueba/MainActivity.java
@@ -2,44 +2,33 @@ package com.example.proyectoprueba;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView text1;
-    String imagePosition = "";
+    private ImageButton image1, image2, image3, image4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        text1 = findViewById(R.id.tex1);
-        int nro = (int)(Math.random() * 8);
-
-        switch (nro) {
-            case 0: imagePosition = "image1";break;
-            case 1: imagePosition = "image2";break;
-            case 2: imagePosition = "image3";break;
-            case 3: imagePosition = "image4";break;
-            case 4: imagePosition = "image5";break;
-            case 5: imagePosition = "image6";break;
-        }
-
-        text1= findViewById(R.id.tex1);
-        text1.setText("Cual es la posicion de:  " + imagePosition);
     }
 
     public void press(View v) {
-        ImageButton b = (ImageButton)v;
-        if (b.getTag().toString().equals(imagePosition)) {
-            Toast.makeText(this, "Correcto", Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(this, "Es incorrecto senalaste la posicion " + b.getTag().toString(), Toast.LENGTH_SHORT).show();
-        }
+        ImageButton imgBtn = (ImageButton)v;
+        imgBtn.setVisibility(View.INVISIBLE);
+    }
+
+    public void restart(View v) {
+        ImageButton image1 = findViewById(R.id.image1);
+        ImageButton image2 = findViewById(R.id.image2);
+        ImageButton image3 = findViewById(R.id.image3);
+        ImageButton image4 = findViewById(R.id.image4);
+
+        image1.setVisibility(View.VISIBLE);
+        image2.setVisibility(View.VISIBLE);
+        image3.setVisibility(View.VISIBLE);
+        image4.setVisibility(View.VISIBLE);
     }
 }
