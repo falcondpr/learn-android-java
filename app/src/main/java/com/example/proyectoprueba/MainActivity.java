403440b483e26803e3a9a939dd3ff9ae786cf27f
Fernando Villalba <21fervillalbag@gmail.com>
2024-04-01 16:17:22 -0400

    feat: Recycler view list of images dados

--- app/src/main/java/com/example/proyectoprueba/MainActivity.java
+++ app/src/main/java/com/example/proyectoprueba/MainActivity.java
@@ -1,63 +1,74 @@
 package com.example.proyectoprueba;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
-    String[] countries={"Argentina","Bolivia","Brasil"};
-    int [] flags = {R.drawable.dado1, R.drawable.dado2, R.drawable.dado3};
-    Spinner spinner;
+    String[] names={"dado1","dado2","dado3","dado4","dado5","dado6"};
+    float[] circlesCount={1,2,3,4,5,6};
+    int[] photos={R.drawable.dado1,R.drawable.dado2,R.drawable.dado3,R.drawable.dado4,R.drawable.dado5,R.drawable.dado6};
+
+    RecyclerView rv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinner = findViewById(R.id.spinner);
-        CountriesAdapter adapter1 = new CountriesAdapter();
-        spinner.setAdapter(adapter1);
+        rv1 = findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rv1.setLayoutManager(linearLayoutManager);
+        rv1.setAdapter(new AdapterDado());
     }
 
-    public void getItemFn(View v) {
-        Toast.makeText(this, spinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
-    }
-
-    class CountriesAdapter extends BaseAdapter {
+    private class AdapterDado extends RecyclerView.Adapter<AdapterDado.AdapterDadoHolder> {
+        @NonNull
         @Override
-        public int getCount() {
-            return countries.length;
+        public AdapterDadoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new AdapterDadoHolder(getLayoutInflater().inflate(R.layout.itemdado, parent, false));
         }
 
         @Override
-        public Object getItem(int position) {
-            return countries[position];
+        public void onBindViewHolder(@NonNull AdapterDadoHolder holder, int position) {
+            holder.printFn(position);
         }
 
         @Override
-        public long getItemId(int position) {
-            return 0;
+        public int getItemCount() {
+            return circlesCount.length;
         }
 
-        @SuppressLint({"ViewHolder", "InflateParams"})
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater inflater  = LayoutInflater.from(MainActivity.this);
-            convertView = inflater.inflate(R.layout.itemspinner, null);
-            ImageView iv1 = convertView.findViewById(R.id.imageView);
-            TextView txt1 = convertView.findViewById(R.id.txtCountry);
-            iv1.setImageResource(flags[position]);
-            txt1.setText(countries[position]);
-            return convertView;
+        private class AdapterDadoHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            TextView txt1, txt2;
+            ImageView img1;
+            public AdapterDadoHolder(@NonNull View itemView) {
+                super(itemView);
+                img1 = itemView.findViewById(R.id.imageView2);
+                txt1 = itemView.findViewById(R.id.txtName1);
+                txt2 = itemView.findViewById(R.id.txtName2);
+
+                itemView.setOnClickListener(this);
+            }
+
+            public void printFn(int position) {
+                img1.setImageResource(photos[position]);
+                txt1.setText(names[position]);
+                txt2.setText(String.valueOf(circlesCount[position]));
+            }
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, names[getLayoutPosition()], Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
