49a6053d1bdaa971ff4716c30a3ede5de45ec34f
Fernando Villalba <21fervillalbag@gmail.com>
2024-04-03 10:07:56 -0400

    feat: creating and reading file.txt

--- app/src/main/java/com/example/proyectoprueba/MainActivity.java
+++ app/src/main/java/com/example/proyectoprueba/MainActivity.java
@@ -1,41 +1,65 @@
 package com.example.proyectoprueba;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 
 public class MainActivity extends AppCompatActivity {
-    TextView text1;
-
-    private ActivityResultLauncher<Intent> loginLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == RESULT_OK) {
-                    Bundle dataResult = result.getData().getExtras();
-                    text1.setText(dataResult.getString("username"));
-                } else if (result.getResultCode() == RESULT_CANCELED) {
-                    Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show();
-                }else {}
-            }
-    );
-
+    EditText input1, input2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text1 = findViewById(R.id.preview1);
+        input1 = findViewById(R.id.input1);
+        input2 = findViewById(R.id.input2);
     }
 
-    public void goToLogin(View v) {
-        Intent intent1 = new Intent(this, FormLogin.class);
-        loginLauncher.launch(intent1);
+    public void save(View v) {
+        String fileName = input1.getText().toString();
+        String content = input2.getText().toString();
+
+        try {
+            OutputStreamWriter newFile = new OutputStreamWriter(openFileOutput(fileName, Context.MODE_PRIVATE));
+            newFile.write(content);
+            newFile.flush();
+            newFile.close();
+            input1.setText("");
+            input2.setText("");
+            Toast.makeText(this, "Los datos fueron grabados", Toast.LENGTH_SHORT).show();
+        } catch(IOException e) {
+            Toast.makeText(this, "No se pudo crear el archivo", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void recover(View v) {
+        String fileName = input1.getText().toString();
+
+        try {
+            InputStreamReader fileExist = new InputStreamReader(openFileInput(fileName));
+            BufferedReader b = new BufferedReader(fileExist);
+            String line_of_file = b.readLine();
+            String content = "";
+            while (line_of_file != null) {
+                content = content + line_of_file+"\n";
+                line_of_file = b.readLine();
+            }
+
+            b.close();
+            fileExist.close();
+            input2.setText(content);
+        } catch(IOException e) {
+            Toast.makeText(this, "No se pudo encontrar el archivo", Toast.LENGTH_SHORT).show();
+        }
     }
 }
