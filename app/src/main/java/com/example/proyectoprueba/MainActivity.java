cf04f5b918103d03833cf101624f353e43b327c2
Fernando Villalba <21fervillalbag@gmail.com>
2024-04-03 14:43:46 -0400

    feat: read, search and write file

--- app/src/main/java/com/example/proyectoprueba/MainActivity.java
+++ app/src/main/java/com/example/proyectoprueba/MainActivity.java
@@ -1,17 +1,13 @@
 package com.example.proyectoprueba;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,40 +22,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void save(View v) {
-        String fileName = input1.getText().toString();
-        String content = input2.getText().toString();
-
-        try {
-            OutputStreamWriter newFile = new OutputStreamWriter(openFileOutput(fileName, Context.MODE_PRIVATE));
-            newFile.write(content);
-            newFile.flush();
-            newFile.close();
-            input1.setText("");
-            input2.setText("");
-            Toast.makeText(this, "Los datos fueron grabados", Toast.LENGTH_SHORT).show();
-        } catch(IOException e) {
-            Toast.makeText(this, "No se pudo crear el archivo", Toast.LENGTH_SHORT).show();
-        }
+        SharedPreferences sp = getSharedPreferences("agenda", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        String date1 = input1.getText().toString();
+        String activityText = input2.getText().toString();
+        editor.putString(date1, activityText);
+        editor.commit();
+        input1.setText("");
+        input2.setText("");
+        Toast.makeText(this, "Las actividades fueron registradas", Toast.LENGTH_SHORT).show();
     }
 
     public void recover(View v) {
-        String fileName = input1.getText().toString();
+        SharedPreferences sp = getSharedPreferences("agenda", Context.MODE_PRIVATE);
+        String dataFile = sp.getString(input1.getText().toString(), "Not found!");
 
-        try {
-            InputStreamReader fileExist = new InputStreamReader(openFileInput(fileName));
-            BufferedReader b = new BufferedReader(fileExist);
-            String line_of_file = b.readLine();
-            String content = "";
-            while (line_of_file != null) {
-                content = content + line_of_file+"\n";
-                line_of_file = b.readLine();
-            }
-
-            b.close();
-            fileExist.close();
-            input2.setText(content);
-        } catch(IOException e) {
-            Toast.makeText(this, "No se pudo encontrar el archivo", Toast.LENGTH_SHORT).show();
+        if (dataFile.equals("Not found!")) {
+            input2.setText("");
+            Toast.makeText(this, "Las actividades NO fueron encontradas", Toast.LENGTH_SHORT).show();
+        } else {
+            input2.setText(dataFile);
         }
     }
 }
